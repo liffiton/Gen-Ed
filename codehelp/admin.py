ec9c2abdacdf0c414588019bd4e1021ca02c297d
Mark Liffiton <liffiton@gmail.com>
2023-02-05 13:02:31 -0600

    Better admin display and filtering.

--- codehelp/admin.py
+++ codehelp/admin.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from flask import Blueprint, current_app, render_template, request, send_file
 
 from .db import get_db
@@ -6,27 +8,56 @@ from .auth import admin_required
 
 bp = Blueprint('admin', __name__, url_prefix="/admin", template_folder='templates')
 
+Filter = namedtuple('Filter', ('name', 'column', 'value'))
+
+
+class Filters:
+    def __init__(self):
+        self._filters = []
+
+    def add_where(self, name, column, value):
+        self._filters.append(Filter(name, column, value))
+
+    def make_where(self):
+        if not self._filters:
+            return "", []
+        else:
+            return (
+                "WHERE " + " AND ".join(f"{f.column}=?" for f in self._filters),
+                [f.value for f in self._filters]
+            )
+
+    def get_filters(self):
+        return self._filters
+
 
 @bp.route("/")
 @admin_required
 def main():
     db = get_db()
     classes = db.execute("SELECT classes.*, COUNT(queries.id) AS num_queries FROM classes LEFT JOIN roles ON roles.class_id=classes.id LEFT JOIN queries ON queries.role_id=roles.id GROUP BY classes.id").fetchall()
-    users = db.execute("SELECT users.*, COUNT(queries.id) AS num_queries FROM users LEFT JOIN queries ON users.id=queries.user_id GROUP BY users.id").fetchall()
-    roles = db.execute("SELECT roles.*, users.username, COUNT(queries.id) AS num_queries FROM roles LEFT JOIN users ON users.id=roles.user_id LEFT JOIN queries ON roles.id=queries.role_id GROUP BY roles.id").fetchall()
 
-    username = None
-    role = None
+    filters = Filters()
+
+    if 'class' in request.args:
+        filters.add_where("class", "roles.class_id", request.args['class'])
+
+    where_clause, where_params = filters.make_where()
+    users = db.execute(f"SELECT users.*, COUNT(queries.id) AS num_queries FROM users LEFT JOIN roles ON roles.user_id=users.id LEFT JOIN queries ON users.id=queries.user_id {where_clause} GROUP BY users.id", where_params).fetchall()
+
     if 'username' in request.args:
-        username = request.args['username']
-        queries = db.execute("SELECT queries.*, users.username FROM queries JOIN users ON queries.user_id=users.id WHERE users.username=? ORDER BY query_time DESC", [username]).fetchall()
-    elif 'role' in request.args:
-        role = request.args['role']
-        queries = db.execute("SELECT queries.*, users.username FROM queries JOIN users ON queries.user_id=users.id WHERE queries.role_id=? ORDER BY query_time DESC", [role]).fetchall()
-    else:
-        queries = db.execute("SELECT queries.*, users.username FROM queries JOIN users ON queries.user_id=users.id ORDER BY query_time DESC").fetchall()
+        filters.add_where("user", "users.username", request.args['username'])
 
-    return render_template("admin.html", classes=classes, users=users, roles=roles, queries=queries, username=username, role=role)
+    where_clause, where_params = filters.make_where()
+    roles = db.execute(f"SELECT roles.*, users.username, COUNT(queries.id) AS num_queries FROM roles LEFT JOIN users ON users.id=roles.user_id LEFT JOIN queries ON roles.id=queries.role_id {where_clause} GROUP BY roles.id", where_params).fetchall()
+
+    if 'role' in request.args:
+        filters.add_where("role", "roles.id", request.args['role'])
+
+    where_clause, where_params = filters.make_where()
+    queries = db.execute(f"SELECT queries.*, users.username FROM queries JOIN users ON queries.user_id=users.id LEFT JOIN roles ON queries.role_id=roles.id {where_clause} ORDER BY query_time DESC", where_params).fetchall()
+
+    return render_template("admin.html", classes=classes, users=users, roles=roles, queries=queries, filters=filters.get_filters())
 
 
 @bp.route("/get_db")
