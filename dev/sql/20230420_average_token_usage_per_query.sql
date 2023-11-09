4b8d29e8f02e6672dca4cccd16b2753cf15f2d9a
Mark Liffiton <liffiton@gmail.com>
2023-11-08 23:22:34 -0600

    Improve avg token usage query.

--- dev/sql/20230420_average_token_usage_per_query.sql
+++ dev/sql/20230420_average_token_usage_per_query.sql
@@ -7,8 +7,9 @@ SELECT
 		   SUM(json_extract(value, '$.usage.prompt_tokens')) AS "Prompt", 
 		   SUM(json_extract(value, '$.usage.completion_tokens')) AS "Completion"
 		 FROM queries, json_each(queries.response_json, '$')
-		 WHERE queries.response_json LIKE "[%"
+		 WHERE queries.response_json LIKE "[{%"
+         AND queries.response_json NOT LIKE "%""]"  -- filter out lists w/ an error message at the end
 		 GROUP BY queries.id
 		 ORDER BY queries.id DESC
-		 LIMIT 250
+		 LIMIT 2000
 	)
