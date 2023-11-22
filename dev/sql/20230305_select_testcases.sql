ad8d1f61a11b2e0a4e1fcc7c722a922acfd3b246
Mark Liffiton <liffiton@gmail.com>
2023-11-22 12:09:21 -0600

    Update testcase puller.

--- dev/sql/20230305_select_testcases.sql
+++ dev/sql/20230305_select_testcases.sql
@@ -1,3 +1,6 @@
-SELECT *, MIN(query_time) FROM queries WHERE language="python" AND query_time > "2023-01-30" AND id % 6 = 0
+.mode csv
+.headers on
+
+SELECT *, MIN(query_time) FROM queries WHERE query_time > date("2023-09-30") AND id % 50 = 0
   GROUP BY IIF(code != "", SUBSTR(code, 0, 5), SUBSTR(issue, 5, 5))
   ORDER BY id;
