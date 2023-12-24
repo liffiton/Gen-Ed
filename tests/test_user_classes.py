512f3267e588f69030c1d118045e899c464eab58
Mark Liffiton <liffiton@gmail.com>
2023-12-23 19:05:37 -0600

    Add test for user class creation.

--- tests/test_user_classes.py
+++ tests/test_user_classes.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 
@@ -24,3 +26,27 @@ def test_user_class_link(auth, client, link_name, status, result):
         assert response.location == result
     elif status == 200:
         assert result in response.text
+
+
+def _create_user_class(client, class_name):
+    response = client.post(
+        "/classes/create/",
+        data={'class_name': class_name, 'openai_key': "none"}
+    )
+
+    assert response.status_code == 302
+    assert response.location == "/instructor/config/"
+
+    response = client.get(response.location)
+    match = re.search(r"https?:\/\/\S+\/classes\/access\/(\w+)", response.text)
+    assert match is not None
+    assert match.group(1)
+
+    class_access_link_name = match.group(1)
+    return class_access_link_name
+
+
+def test_user_class_creation(auth, client):
+    auth.login()  # only works if logged in
+    class_access_link_name = _create_user_class(client, "Test Class")
+    test_user_class_link(auth, client, class_access_link_name, 302, '/')
