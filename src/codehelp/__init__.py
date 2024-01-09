9f75a4e70c3886754fc7df5db39de9e067bd693a
Mark Liffiton <liffiton@gmail.com>
2024-01-08 20:12:22 -0600

    Add favicon to Codehelp.

--- src/codehelp/__init__.py
+++ src/codehelp/__init__.py
@@ -21,6 +21,7 @@ def create_app(test_config: dict[str, Any] | None = None, instance_path: Path | 
     app_config = dict(
         APPLICATION_TITLE='CodeHelp',
         APPLICATION_AUTHOR='Mark Liffiton',
+        FAVICON='icon.png',
         SUPPORT_EMAIL='support@codehelp.app',
         HELP_LINK_TEXT='Get Help',
         DATABASE_NAME='codehelp.db',  # will be combined with app.instance_path in gened.create_app_base()
