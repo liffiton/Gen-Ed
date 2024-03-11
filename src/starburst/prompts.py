7fe0ba1ce780a7abec76fcfa2d5118a60b838be2
Mark Liffiton <liffiton@gmail.com>
2024-03-11 14:29:33 -0500

    Tweak starburst prompt.

--- src/starburst/prompts.py
+++ src/starburst/prompts.py
@@ -8,9 +8,9 @@ import random
 def make_main_prompt(assignment: str, topics: str) -> str:
     nonce = random.randint(1000, 9999)
     return f"""You are a system for assisting students with finding topics for papers they are writing in college.
-The students provide:
- - a copy of the prompt from the writing assignment they are working on (in "<assignment_{nonce}>" delimiters)
- - a set of topics they are interested in so far (in "<topics_{nonce}>" delimiters)
+As a student, I provide:
+ - a copy of the prompt from the writing assignment I am working on (in "<assignment_{nonce}>" delimiters)
+ - a set of topics I am interested in so far (in "<topics_{nonce}>" delimiters)
 
 <assignment_{nonce}>
 {assignment}
@@ -19,12 +19,10 @@ The students provide:
 {topics}
 </topics_{nonce}>
 
-Assist the student in brainstorming topics and themes for their paper.
+Assist me in brainstorming topics and themes for my paper.
 
 1. For each of the individual topics the student mentions, suggest a set of keywords, phrases, or concepts that relate to that topic.  Make sure they are within the scope of the assignment prompt, if it places any constraints on the student's choice of topic.
-2. Produce a set of keywords, phrases, or concepts that tie together the student's topics in some way as well.
+2. Produce a varied set of keywords, phrases, and/or concepts that tie together my topics in some way as well.  Explain the connections if they are not self-evident.
 
-Use Markdown formatting.  Place a blank line before the start of any list, and indent each additional nested level by four more spaces.
-
-Do not reproduce the student's inputs.
+Use Markdown formatting.
 """
