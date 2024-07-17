4e64d24404a5d06e3910fa25d65cd98c0a683ce4
Mark Liffiton <liffiton@gmail.com>
2024-07-17 00:37:59 -0500

    New landing page.

--- tests/test_landing.py
+++ tests/test_landing.py
@@ -5,4 +5,4 @@
 def test_landing(client):
     '''Make sure we get the landing page from a basic root path request.'''
     response = client.get('/')
-    assert b"CodeHelp is a coding and CS assistant." in response.data
+    assert "Automated Teaching Assistant" in response.text
